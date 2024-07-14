aef3187bebbf8272dee0eeb4608b7ff6a9e8a377
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-07-14 14:59:37 +0100

    Compute avg score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,19 +1,19 @@
--- Procedure AddBonus: Computes and stores the average score for a student.
+-- Procedure ComputeAverageScoreForUser: Computes and stores the average score for a student.
 DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
+    IN p_user_id INT
 )
 BEGIN
     DECLARE avg_score FLOAT;
 
     SELECT AVG(score) INTO avg_score
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = p_user_id;
 
     UPDATE users
     SET average_score = avg_score
-    WHERE id = user_id;
+    WHERE id = p_user_id;
 END $$
 
 DELIMITER ;
