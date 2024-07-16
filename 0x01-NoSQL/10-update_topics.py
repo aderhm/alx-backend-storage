6892f39d7c49cdbd8c4a88b51177dfc4fecc05ec
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-07-16 15:23:05 +0100

    Update a doc

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module: Changes all topics of a school document
+"""
+
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """Changes all topics of a school document based on the name
+    """
+    filter = {"name": name}
+    update = {"$set": {"topics": topics}}
+    mongo_collection.update_one(filter, update)
