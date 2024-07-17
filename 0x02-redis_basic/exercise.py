1cf1c5a91d6fac9818f2a77a3e5369bb55bd6575
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-07-17 22:05:48 +0100

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,46 +1,61 @@
 #!/usr/bin/env python3
-"""Module: Class Cache
-"""
+"""Module: Writing strings to Redis"""
 
 import redis
 import uuid
-from typing import Union, Callable, Optional
+from functools import wraps
+from typing import Union, Callable
+
+
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """Gets the qualified name of the method
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+    return wrapper
 
 
 class Cache:
-    """Class Cache
-    """
-
+    """Class Cache"""
     def __init__(self):
-        """Intialize class Cache
+        """Initialization
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Sets a data to a key
+        """Generates a random uuid key and stores the input data in Redis
+        using the random key and return the key
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
-            str, bytes, int, float, None]:
-        """Reads from Redis
+    def get(
+            self, key: str, fn: Callable = None
+            ) -> Union[str, bytes, int, float, None]:
+        """Gets data from Redis
         """
         data = self._redis.get(key)
+
         if data is None:
             return None
-        if fn:
+
+        if fn is not None:
             return fn(data)
+
         return data
 
-    def get_str(self, key: str) -> Optional[str]:
-        """Recovers original type
+    def get_str(self, key: str) -> Union[str, None]:
+        """Gets data from Redis and returns it as str
         """
         return self.get(key, lambda x: x.decode('utf-8'))
 
-    def get_int(self, key: str) -> Optional[int]:
-        """Recovers original type
+    def get_int(self, key: str) -> Union[int, None]:
+        """Gets data from Redis and returns it as int
         """
-        return self.get(key, lambda x: int(x))
+        return self.get(key, int)
