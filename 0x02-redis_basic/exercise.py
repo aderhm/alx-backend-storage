aa333d97c4e8d4b67aba516b0c033d7460d11956
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-07-17 19:45:57 +0100

    Write a string to redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python3
+"""Module: Class Cache
+"""
 
+import redis
+import uuid
+
+
+class Cache:
+    """Class Cache
+    """
+
+    def __init__(self):
+        """Intialize class Cache
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: str | bytes | int | float) -> str:
+        """Sets a data to a key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
